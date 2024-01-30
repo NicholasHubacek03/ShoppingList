61f22af22ee2db3464c96c31544666745aafab79
Nicholas <Nicholas.Hubacek@gmail.com>
2024-01-30 15:14:25 -0600

    fixed title

--- App.tsx
+++ App.tsx
@@ -8,7 +8,9 @@ export default function App() {
         source={require('./images/grocey-background.jpg')}
         style={styles.imageBackground}  // Use the style prop here
       >
-        <Text style={styles.text}>Shopping List</Text>
+ <View style={styles.titleContainer}>
+          <Text style={styles.text}>Shopping List</Text>
+        </View>
         <StatusBar style="auto" />
       </ImageBackground>
     </View>
@@ -18,12 +20,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
   },
   imageBackground: {
     flex: 1,
     resizeMode: 'cover',
-    justifyContent: 'center',
-    
+
+  },
+    titleContainer: {
+      paddingTop: 50, // Adjust the paddingTop to move the title down from the top
+      alignItems: 'center',
   },
   text: {
     color: 'white',
