7533fa1b8d5dee6e4857f25ffa813fdb9a8c4a8f
Nicholas <Nicholas.Hubacek@gmail.com>
2024-01-31 11:50:30 -0600

    added basic buttons and names

--- App.tsx
+++ App.tsx
@@ -1,16 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
+
+interface ItoShop{
+  text: string;
+  completed: boolean;
+}
 
 export default function App() {
+
+  const [value, setValue] = useState<string>("");
+  const [toDoShop, setToShops] = useState<ItoShop[]>([]);
+  const [error, showError] = useState<boolean>(false);
+  
   return (
     <View style={styles.container}>
       <ImageBackground 
-        source={require('./images/grocey-background.jpg')}
-        style={styles.imageBackground}  // Use the style prop here
-      >
- <View style={styles.titleContainer}>
-          <Text style={styles.text}>Shopping List</Text>
-        </View>
+          source={require('./images/grocey-background.jpg')}
+            style={styles.imageBackground}
+                                          >
+            <View style={styles.titleContainer}>
+              <Text style={styles.text}>Shopping List</Text>
+                  <TextInput 
+                    placeholder='Enter shopping items'
+                      value=''
+                        onChange={e => {}}
+                                          />
+          <Button title='add Grocery' onPress={() => {}}/>
+            </View>
+              {error && <Text>Error: Input field is empty...</Text>}
+              <Text>Your Shopping List :</Text>
         <StatusBar style="auto" />
       </ImageBackground>
     </View>
